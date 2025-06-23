340090765afb436da9b3fc473d5dfb1e072585e0
Nirmal Samaranayaka <nirmal.fullstack@gmail.com>
2025-06-23 14:53:32 +0200

    Bulk send details table and route link with menu link updating

--- customer-care-ticketing-system-frontend/src/app/app-routing.module.ts
+++ customer-care-ticketing-system-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { HomeComponent } from './home/home.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { TicketNotFoundComponent } from './ticket-not-found/ticket-not-found.component';
+import { BulkSendHistoryComponent } from './bulk-send-history/bulk-send-history.component';
 
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent, children: [
       { path: 'tickets/not-found', component: TicketNotFoundComponent },
       { path: 'tickets/:ticketId', component: TicketComponent },
+      { path: 'bulk-send-history', component: BulkSendHistoryComponent },
     ]
   },
   { path: '**', redirectTo: 'home' }
